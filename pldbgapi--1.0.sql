acbc10408409dc5bf1dff0778d733495a55c6869
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2012-04-12 13:25:33 +0300

    Using CREATE OR REPLACE is not recommended in extension install scripts.

--- pldbgapi--1.0.sql
+++ pldbgapi--1.0.sql
@@ -18,26 +18,26 @@ CREATE TYPE targetinfo AS ( target OID, schema OID, nargs INT, argTypes oidvecto
 CREATE TYPE var		   AS ( name TEXT, varClass char, lineNumber INTEGER, isUnique bool, isConst bool, isNotNull bool, dtype OID, value TEXT );
 CREATE TYPE proxyInfo  AS ( serverVersionStr TEXT, serverVersionNum INT, proxyAPIVer INT, serverProcessID INT );
 
-CREATE OR REPLACE FUNCTION plpgsql_oid_debug( functionOID OID ) RETURNS INTEGER AS '$libdir/plugins/plugin_debugger' LANGUAGE C STRICT;
+CREATE FUNCTION plpgsql_oid_debug( functionOID OID ) RETURNS INTEGER AS '$libdir/plugins/plugin_debugger' LANGUAGE C STRICT;
 
-CREATE OR REPLACE FUNCTION pldbg_abort_target( session INTEGER ) RETURNS SETOF boolean AS  '$libdir/pldbgapi' LANGUAGE C STRICT;
-CREATE OR REPLACE FUNCTION pldbg_attach_to_port( portNumber INTEGER ) RETURNS INTEGER AS '$libdir/pldbgapi' LANGUAGE C STRICT;
-CREATE OR REPLACE FUNCTION pldbg_continue( session INTEGER ) RETURNS breakpoint AS '$libdir/pldbgapi' LANGUAGE C STRICT;
-CREATE OR REPLACE FUNCTION pldbg_create_listener() RETURNS INTEGER AS '$libdir/pldbgapi' LANGUAGE C STRICT;
-CREATE OR REPLACE FUNCTION pldbg_deposit_value( session INTEGER, varName TEXT, lineNumber INTEGER, value TEXT ) RETURNS boolean AS  '$libdir/pldbgapi' LANGUAGE C STRICT;
-CREATE OR REPLACE FUNCTION pldbg_drop_breakpoint( session INTEGER, func OID, linenumber INTEGER ) RETURNS boolean AS  '$libdir/pldbgapi' LANGUAGE C STRICT;
-CREATE OR REPLACE FUNCTION pldbg_get_breakpoints( session INTEGER ) RETURNS SETOF breakpoint AS '$libdir/pldbgapi' LANGUAGE C STRICT;
-CREATE OR REPLACE FUNCTION pldbg_get_source( session INTEGER, func OID ) RETURNS TEXT AS '$libdir/pldbgapi' LANGUAGE C STRICT;
-CREATE OR REPLACE FUNCTION pldbg_get_stack( session INTEGER ) RETURNS SETOF frame AS '$libdir/pldbgapi' LANGUAGE C STRICT;
-CREATE OR REPLACE FUNCTION pldbg_get_proxy_info( ) RETURNS proxyInfo AS '$libdir/pldbgapi' LANGUAGE C STRICT;
-CREATE OR REPLACE FUNCTION pldbg_get_variables( session INTEGER ) RETURNS SETOF var AS '$libdir/pldbgapi' LANGUAGE C STRICT;
-CREATE OR REPLACE FUNCTION pldbg_select_frame( session INTEGER, frame INTEGER ) RETURNS breakpoint AS '$libdir/pldbgapi' LANGUAGE C STRICT;
-CREATE OR REPLACE FUNCTION pldbg_set_breakpoint( session INTEGER, func OID, linenumber INTEGER ) RETURNS boolean AS  '$libdir/pldbgapi' LANGUAGE C STRICT;
-CREATE OR REPLACE FUNCTION pldbg_set_global_breakpoint( session INTEGER, func OID, linenumber INTEGER, targetPID INTEGER ) RETURNS boolean AS  '$libdir/pldbgapi' LANGUAGE C;
-CREATE OR REPLACE FUNCTION pldbg_step_into( session INTEGER ) RETURNS breakpoint AS '$libdir/pldbgapi' LANGUAGE C STRICT;
-CREATE OR REPLACE FUNCTION pldbg_step_over( session INTEGER ) RETURNS breakpoint AS '$libdir/pldbgapi' LANGUAGE C STRICT;
-CREATE OR REPLACE FUNCTION pldbg_wait_for_breakpoint( session INTEGER ) RETURNS breakpoint  AS '$libdir/pldbgapi' LANGUAGE C STRICT;
-CREATE OR REPLACE FUNCTION pldbg_wait_for_target( session INTEGER ) RETURNS INTEGER AS '$libdir/pldbgapi' LANGUAGE C STRICT;
-CREATE OR REPLACE FUNCTION pldbg_get_target_info( signature TEXT, targetType "char" ) RETURNS targetInfo AS '$libdir/targetinfo' LANGUAGE C STRICT;
+CREATE FUNCTION pldbg_abort_target( session INTEGER ) RETURNS SETOF boolean AS  '$libdir/pldbgapi' LANGUAGE C STRICT;
+CREATE FUNCTION pldbg_attach_to_port( portNumber INTEGER ) RETURNS INTEGER AS '$libdir/pldbgapi' LANGUAGE C STRICT;
+CREATE FUNCTION pldbg_continue( session INTEGER ) RETURNS breakpoint AS '$libdir/pldbgapi' LANGUAGE C STRICT;
+CREATE FUNCTION pldbg_create_listener() RETURNS INTEGER AS '$libdir/pldbgapi' LANGUAGE C STRICT;
+CREATE FUNCTION pldbg_deposit_value( session INTEGER, varName TEXT, lineNumber INTEGER, value TEXT ) RETURNS boolean AS  '$libdir/pldbgapi' LANGUAGE C STRICT;
+CREATE FUNCTION pldbg_drop_breakpoint( session INTEGER, func OID, linenumber INTEGER ) RETURNS boolean AS  '$libdir/pldbgapi' LANGUAGE C STRICT;
+CREATE FUNCTION pldbg_get_breakpoints( session INTEGER ) RETURNS SETOF breakpoint AS '$libdir/pldbgapi' LANGUAGE C STRICT;
+CREATE FUNCTION pldbg_get_source( session INTEGER, func OID ) RETURNS TEXT AS '$libdir/pldbgapi' LANGUAGE C STRICT;
+CREATE FUNCTION pldbg_get_stack( session INTEGER ) RETURNS SETOF frame AS '$libdir/pldbgapi' LANGUAGE C STRICT;
+CREATE FUNCTION pldbg_get_proxy_info( ) RETURNS proxyInfo AS '$libdir/pldbgapi' LANGUAGE C STRICT;
+CREATE FUNCTION pldbg_get_variables( session INTEGER ) RETURNS SETOF var AS '$libdir/pldbgapi' LANGUAGE C STRICT;
+CREATE FUNCTION pldbg_select_frame( session INTEGER, frame INTEGER ) RETURNS breakpoint AS '$libdir/pldbgapi' LANGUAGE C STRICT;
+CREATE FUNCTION pldbg_set_breakpoint( session INTEGER, func OID, linenumber INTEGER ) RETURNS boolean AS  '$libdir/pldbgapi' LANGUAGE C STRICT;
+CREATE FUNCTION pldbg_set_global_breakpoint( session INTEGER, func OID, linenumber INTEGER, targetPID INTEGER ) RETURNS boolean AS  '$libdir/pldbgapi' LANGUAGE C;
+CREATE FUNCTION pldbg_step_into( session INTEGER ) RETURNS breakpoint AS '$libdir/pldbgapi' LANGUAGE C STRICT;
+CREATE FUNCTION pldbg_step_over( session INTEGER ) RETURNS breakpoint AS '$libdir/pldbgapi' LANGUAGE C STRICT;
+CREATE FUNCTION pldbg_wait_for_breakpoint( session INTEGER ) RETURNS breakpoint  AS '$libdir/pldbgapi' LANGUAGE C STRICT;
+CREATE FUNCTION pldbg_wait_for_target( session INTEGER ) RETURNS INTEGER AS '$libdir/pldbgapi' LANGUAGE C STRICT;
+CREATE FUNCTION pldbg_get_target_info( signature TEXT, targetType "char" ) RETURNS targetInfo AS '$libdir/targetinfo' LANGUAGE C STRICT;
 
 
