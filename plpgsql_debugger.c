c719ff4d50e2bb3e57b5c81690c65de8e0334997
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2012-04-25 11:22:32 +0300

    Clear out unused fields in PLpgSQL_expr
    This makes the code more robust, avoiding uninitialized fields if new fields
    are added to the struct in the future. Initializing them to zeros might not
    be right either, but it's a good guess and better than leaving them
    uninitialized.

--- plpgsql_debugger.c
+++ plpgsql_debugger.c
@@ -967,7 +967,7 @@ static bool plpgsql_do_deposit(ErrorContextCallback *frame, const char *var_name
 	 *	     transaction so we don't free it out from under spl_plpgsql_xact_cb()
 	 */
 
-	expr = (PLpgSQL_expr *) palloc( sizeof( *expr ));
+	expr = (PLpgSQL_expr *) palloc0( sizeof( *expr ));
 
 	expr->dtype       	   = PLPGSQL_DTYPE_EXPR;
 	expr->dno              = -1;
