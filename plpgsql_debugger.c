d026fabeef7f534a8a046624dd48ba45e7a79a0f
lm1277797-lgtm <lm1277797@gmail.com>
2026-03-30 16:35:06 -0600

    Revert "Fix compatibility with pg18"

--- plpgsql_debugger.c
+++ plpgsql_debugger.c
@@ -681,7 +681,11 @@ print_rec(const PLpgSQL_execstate *frame, const char *var_name, int lineno,
 	{
 		char * extval = SPI_getvalue( tuple, rec_tupdesc, attNo + 1 );
 
-		dbg_send( "v:%s.%s:%s\n", var_name, NameStr( TupleDescAttr(rec_tupdesc, attNo)->attname ), extval ? extval : "NULL" );
+#if (PG_VERSION_NUM >= 110000)
+		dbg_send( "v:%s.%s:%s\n", var_name, NameStr( rec_tupdesc->attrs[attNo].attname ), extval ? extval : "NULL" );
+#else
+		dbg_send( "v:%s.%s:%s\n", var_name, NameStr( rec_tupdesc->attrs[attNo]->attname ), extval ? extval : "NULL" );
+#endif
 
 		if( extval )
 			pfree( extval );
